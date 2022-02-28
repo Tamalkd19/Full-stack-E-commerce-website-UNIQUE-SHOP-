0f6a9525751525b1164707a74396f1844131e019
Tamal kanti  das <tamalkd19@gmail.com>
2022-02-28 09:17:50 +0530

    Uniqueshop1

---
+++ frontend/build/precache-manifest.0e08082e650a440498e3497fbdc7925b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49010fee6f5e54de45d1043929d3157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bed91b1afaddee5e5f03",
+    "url": "/static/css/main.4100198a.chunk.css"
+  },
+  {
+    "revision": "4de9ada1268d5f63f86c",
+    "url": "/static/js/2.1437b863.chunk.js"
+  },
+  {
+    "revision": "dd91769caa8a7c882f17",
+    "url": "/static/js/3.8e94edcc.chunk.js"
+  },
+  {
+    "revision": "bed91b1afaddee5e5f03",
+    "url": "/static/js/main.2bfbe1c9.chunk.js"
+  },
+  {
+    "revision": "b1b80c4c074a2fcd431b",
+    "url": "/static/js/runtime~main.90ed1313.js"
+  }
+]);
